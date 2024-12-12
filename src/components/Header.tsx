5f90b903e33975c89bfdd682d5f6617dcb287f7d
MiniGT <bendanillo539@gmail.com>
2024-12-12 23:01:33 +0800

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+  return (
+  <nav className="navbar bg-[#fff]">
+    <div className="flex gap-2 justify-center items-center py-2">
+      <img className="w-10" src="/chef-claude.svg"/>
+      <div className="text-2xl">Chef Claude</div>
+    </div>
+  </nav>
+  )
+}
