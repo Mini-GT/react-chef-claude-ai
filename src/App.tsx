0808f45c791f2e7910186848d210e4fd6f2de48e
MiniGT <bendanillo539@gmail.com>
2024-12-12 23:02:20 +0800

    imported components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Main from "./components/Main"
+
 function App() {
 
   return (
-    <>
-      <div className="bg-red-600">Div</div>
-    </>
+    <div className="bg-[#FAFAF8] h-screen">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   )
 }
 
