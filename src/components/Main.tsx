57586bee62b15f1a6efd6a4b1911c20eb6264369
MiniGT <bendanillo539@gmail.com>
2025-01-03 21:55:19 +0800

    revert from react 19 to react 18 form handler and imported fieldset employment component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,34 +1,36 @@
 import { useState } from "react";
+import Fieldset from "./Fieldset";
 
 export default function Main() {
   const [ingredients, setIngredients] = useState<string[]>([])
 
-  const addIngredient = (formData: FormData) => {
-    const newIngredient = formData.get("ingredient") as string
+  // const addIngredient = (formData: FormData) => {
+  //   const newIngredient = formData.get("ingredient") as string
+  //   console.log(newIngredient)
+  //   if(!newIngredient) {
+  //     throw Error("Ingredient empty")
+  //   }
 
-    if(!newIngredient) {
-      throw Error("Ingredient empty")
-    }
-
-    setIngredients(prevIngredient => [...prevIngredient, newIngredient])
-  }
+  //   setIngredients(prevIngredient => [...prevIngredient, newIngredient])
+  // }
 
   // refactor to use form action instead onSubmit attribute:
-  // const handleAddIngredient = (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault()
-  //   const formData = new FormData(event.currentTarget)
-  //   const addNewIngredient = formData.get("ingredient") as string
-  //   setIngredients((prevIngredients) => [...prevIngredients, addNewIngredient]);
-  // }
+  const handleAddIngredient = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const addNewIngredient = formData.get("ingredient") as string
+    setIngredients((prevIngredients) => [...prevIngredients, addNewIngredient]);
+  }
 
   const newIngredients = ingredients.map((ingredient, i) => <li key={i}>{ingredient}</li>)
   
   return (
     <main className="laptop:mx-20 tablet:mx-10 mx-4">
       <form 
-        className="tablet:my-12 my-6 flex items-center gap-4" 
-        action={addIngredient}
-        // onSubmit={handleAddIngredient}
+        className="tablet:my-12 my-6 flex items-center gap-4"
+        // this FORM ACTION is for React 19 only and React Vite is still using React 18
+        //action={addIngredient}
+        onSubmit={handleAddIngredient}
       >
         <input 
           className="tablet:p-2 tablet:w-[50%] py-1 h-full w-full border border-[#6B7280] rounded-md" 
@@ -37,6 +39,7 @@ export default function Main() {
           placeholder="e.g. oregano" 
           aria-label="Add ingredient" 
         />
+
         <button 
           className="tablet:text-xl bg-[#141413] text-white h-full px-2 py-1 rounded-md text-xs text-nowrap border border-black" 
           type="submit" 
@@ -52,6 +55,8 @@ export default function Main() {
           {newIngredients}
         </ul>
       </div>
+
+      <Fieldset />
     </main>
   )
 }
