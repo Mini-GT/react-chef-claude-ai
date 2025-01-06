73fb25134de4c77629058f10d948b543a802f38c
MiniGT <bendanillo539@gmail.com>
2025-01-06 22:11:52 +0800

    test input checkbox

--- src/components/Fieldset.tsx
+++ src/components/Fieldset.tsx
@@ -3,12 +3,14 @@ export default function Fieldset() {
     event.preventDefault()
     const formData = new FormData(event.currentTarget)
     const employmentStatus = formData.get("employmentStatus") as string
+    const dietaryStatus = formData.getAll("dietaryStatus") as string[] // use .getAll to get all values
     console.log(employmentStatus)
+    console.log(dietaryStatus)
   }
 
   return (
     <form onSubmit={handleGetEmploymentStatus}>
-      <fieldset className="flex flex-col">
+      <fieldset className="flex flex-col my-5">
         <legend>Employment Status:</legend>
         <label>
           <input type="radio" name="employmentStatus" value="Unemployed" />
@@ -21,6 +23,22 @@ export default function Fieldset() {
           <label>
             <input type="radio" name="employmentStatus" value="Full-time" />
             Full-time
+        </label>
+      </fieldset>
+
+      <fieldset className="flex flex-col my-5">
+        <legend>Dietary:</legend>
+        <label>
+          <input type="checkbox" name="dietaryStatus" value="kosher" />
+          Kosher
+        </label>
+          <label>
+            <input type="checkbox" name="dietaryStatus" value="vegan" />
+            Vegan
+        </label>
+          <label>
+            <input type="checkbox" name="dietaryStatus" value="gluten-free" />
+            Gluten-Free
         </label>
       </fieldset>
 
