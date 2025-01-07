bbd329fa80e1f95ccad71b3ab45a36e8f93e8c0c
MiniGT <bendanillo539@gmail.com>
2025-01-07 22:06:43 +0800

    renders footer component if ingredient has more than 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Main from "./components/Main"
+import { useStoreIngredient } from "./store/ingredient.store"
 
 function App() {
-
+  const ingredients = useStoreIngredient((state) => state.ingredients)
+  console.log(ingredients.length)
   return (
     <div className="bg-[#FAFAF8] h-screen">
       <Header />
       <Main />
-      <Footer />
+      {ingredients.length > 0 ? <Footer /> : null}
     </div>
   )
 }
