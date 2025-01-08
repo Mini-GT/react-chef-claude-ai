74f62f0af54a38ffba37d0d2ee6006b536ac2474
MiniGT <bendanillo539@gmail.com>
2025-01-08 22:12:50 +0800

    changed storeIngredient type to accept an array of ingredients instead of 1 ingredient

--- src/store/ingredient.store.ts
+++ src/store/ingredient.store.ts
@@ -2,10 +2,10 @@ import {create} from 'zustand'
 
 type Ingredients = {
   ingredients: string[]
-  storeIngredient: (newIngredient: string) => void
+  storeIngredient: (newIngredient: string[]) => void
 }
 
 export const useStoreIngredient = create<Ingredients>((set) => ({
   ingredients: [],
-  storeIngredient: (newIngredient: string) => set((state) => ({ ingredients: [...state.ingredients, newIngredient] })),
+  storeIngredient: (newIngredient: string[]) => set((state) => ({ ingredients: [...state.ingredients, ...newIngredient] })),
 }))
