345a09fae7953d018f0f950f0912fed95129b4f3
MiniGT <bendanillo539@gmail.com>
2025-01-08 23:26:27 +0800

    feat: Get Recipe functional with zustand

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,9 @@
+import { useStoreIsRecipeShown } from '../store/recipeShow.store'
+
 export default function Footer() {
+  const { isRecipeShown, storeIsRecipeShown } = useStoreIsRecipeShown()
+  console.log(isRecipeShown)
+
   return (
     <footer className="laptop:mt-40 laptop:mx-20 tablet:mx-10 bg-[#F0EFEB] rounded-md mx-4 mt-10">
       <div className="flex items-center justify-between gap-3 p-4">
@@ -7,7 +12,10 @@ export default function Footer() {
           <p className="tablet:text-xl text-[#6B7280]">Generate a recipe from your list of ingredients.</p>
         </div>
         <div>
-          <button className="tablet:text-xl text-nowrap text-xs bg-[#D17557] px-2 py-2 rounded-md">Get a recipe</button>
+          <button 
+            className="tablet:text-xl text-nowrap text-xs bg-[#D17557] px-2 py-2 rounded-md"
+            onClick={storeIsRecipeShown}
+          >Get a recipe</button>
         </div>
       </div>
     </footer>
