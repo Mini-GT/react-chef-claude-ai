85a3a3ac9d08e8b4444ef13670b9b4bc3365a924
MiniGT <bendanillo539@gmail.com>
2025-01-09 18:47:35 +0800

    imported recommends component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Main from "./components/Main"
+import Recommends from "./components/Recommends"
 import { useStoreIngredient } from "./store/ingredient.store"
 
 function App() {
   const ingredients = useStoreIngredient((state) => state.ingredients)
-  console.log(ingredients.length)
+  console.log(ingredients)
   return (
     <div className="bg-[#FAFAF8] h-screen">
       <Header />
       <Main />
       {ingredients.length > 3 ? <Footer /> : null}
+      <Recommends />
     </div>
   )
 }
