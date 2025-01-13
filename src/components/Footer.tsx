3a9e539072f5d6207f8ec7087607ba884919e1bd
MiniGT <bendanillo539@gmail.com>
2025-01-13 22:24:35 +0800

    getRecipe handler and storing it with zustand

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,18 @@
-import { useStoreIsRecipeShown } from '../store/recipeShow.store'
+// import { useStoreIsRecipeShown } from '../store/recipeShow.store'
+import { useStoreIngredient } from '../store/ingredient.store'
+import { getRecipeFromMistral } from '../../AI'
+import { useStoreResponseIngredient } from '../store/responseIngredient.store'
 
 export default function Footer() {
-  const { isRecipeShown, storeIsRecipeShown } = useStoreIsRecipeShown()
-  console.log(isRecipeShown)
+  // const { storeIsRecipeShown } = useStoreIsRecipeShown()
+  const {ingredients} = useStoreIngredient((state) => state)
+  const {storeResponseIngredient} = useStoreResponseIngredient((state) => state)
+
+  const getRecipe = async () => {
+    const res = await getRecipeFromMistral(ingredients)
+    storeResponseIngredient(res)
+  }
+
 
   return (
     <footer className="laptop:mt-40 laptop:mx-20 tablet:mx-10 bg-[#F0EFEB] rounded-md mx-4 mt-10">
@@ -14,7 +24,7 @@ export default function Footer() {
         <div>
           <button 
             className="tablet:text-xl text-nowrap text-xs bg-[#D17557] px-2 py-2 rounded-md"
-            onClick={storeIsRecipeShown}
+            onClick={getRecipe}
           >Get a recipe</button>
         </div>
       </div>
